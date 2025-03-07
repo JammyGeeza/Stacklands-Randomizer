3d53ae82e68b06c9620af8c7a6f8a8a9e50b3451
JammyGeeza <jammiedodgers6@hotmail.com>
2025-03-07 16:19:25 +0000

    UPDATE: Items added to item pool, Trap items now supported,  changes to card-blocking logic and "basic cards" list expanded

--- Enums/ItemType.cs
+++ Enums/ItemType.cs
@@ -10,5 +10,6 @@ namespace Stacklands_Randomizer_Mod
         BoosterPack = 0, // Booster Pack unlocks
         Idea = 1, // Blueprints
         Resource = 2, // Resource Cards
+        Trap = 3,
     }
 }
